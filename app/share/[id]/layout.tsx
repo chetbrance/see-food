96fb4d17b39ab88a902f116ecea81e366572996f
Brett Chance <brett.e.chance@gmail.com>
2025-03-26 15:36:08 -0700

    bug fix

--- app/share/[id]/layout.tsx
+++ app/share/[id]/layout.tsx
@@ -1,30 +1,24 @@
 import type { Metadata } from 'next';
 
-export async function generateMetadata(
-  { params }: { params: { id: string } }
-): Promise<Metadata> {
-  const id = params.id;
-  
-  return {
-    title: 'SeeFood Hot Dog Detector Results',
+// Static metadata that will work for all share pages
+export const metadata: Metadata = {
+  title: 'SeeFood Hot Dog Detector Results',
+  description: 'Check out this food photo analyzed by SeeFood Hot Dog Detector!',
+  openGraph: {
+    title: 'SeeFood Hot Dog Detection Results',
     description: 'Check out this food photo analyzed by SeeFood Hot Dog Detector!',
-    openGraph: {
-      title: 'SeeFood Hot Dog Detection Results',
-      description: 'Check out this food photo analyzed by SeeFood Hot Dog Detector!',
-      url: `https://hotdogdetector.com/share/${id}`,
-      siteName: 'SeeFood',
-      locale: 'en_US',
-      type: 'website',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: 'SeeFood Hot Dog Detection Results',
-      description: 'Check out this food photo analyzed by SeeFood Hot Dog Detector!',
-      creator: '@seefood',
-      site: '@seefood',
-    }
-  };
-}
+    siteName: 'SeeFood',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'SeeFood Hot Dog Detection Results',
+    description: 'Check out this food photo analyzed by SeeFood Hot Dog Detector!',
+    creator: '@seefood',
+    site: '@seefood',
+  }
+};
 
 export default function ShareLayout({
   children,
